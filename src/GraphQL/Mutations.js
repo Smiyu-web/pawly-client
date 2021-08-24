39796363bd7b9335748b08985ed7f96ae288f785
Miu <miyuuu0728@gmail.com>
2021-08-24 16:51:52 -0700

    added update

--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -1,8 +1,14 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_WEIGHT_MUTATION = gql`
-  mutation Weight($weightNum: Float!) {
+  mutation Weight($weightNum: Float!, $day: String!, $updateWeightNum: Float!) {
     addWeight(weightNum: $weightNum) {
+      id
+      weightNum
+      day
+      createdAt
+    }
+    updateWeight(day: $day, updateWeightNum: $updateWeightNum) {
       id
       weightNum
       day
