0eeae7c0633bad788e2249f1763e57121dace311
Miu <miyuuu0728@gmail.com>
2021-08-27 00:35:35 -0700

    change charSet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
