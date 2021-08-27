4cbc3630f70bcc08f2af8eb50ee3d448e0b30da9
Miu <miyuuu0728@gmail.com>
2021-08-27 00:34:50 -0700

    added author

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,6 @@ class MyDocument extends Document {
         <Head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <link rel="shortcut icon" href="/favicon.ico" />
